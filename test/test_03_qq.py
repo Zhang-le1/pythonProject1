d1191502e53e14b1d7d6bf041f60ad131f7f3622
zhangle <zhangle@192.168.0.103>
2021-11-21 18:59:22 +0800

    python练习提交，变量、if语句

---
+++ test/test_03_qq.py
@@ -0,0 +1,12 @@
+# 1. 定义一个变量记录qq号码
+qq_number = "1234567"
+
+# 2. 定义一个变量记录qq密码
+qq_password = "123"
+
+# 如果希望通过解释器的方式，输出变量的内容，需要使用print函数
+# 注意：在使用解释器执行python 程序的时候，不能直接使用变量名在控制台输出变量的信息
+
+print(qq_number)
+
+print(qq_password)
